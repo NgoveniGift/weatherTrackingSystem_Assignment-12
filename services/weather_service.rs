463e4b48c8ba6561827bfdd6b0ca9f606307e440
NgoveniGift <139237390+NgoveniGift@users.noreply.github.com>
2025-05-03 04:16:54 -0700

    Update weather_service.rs

--- services/weather_service.rs
+++ services/weather_service.rs
@@ -1,1 +1,30 @@
+// services/weather_service.rs
+
+use crate::repository_interface::{WeatherReport, WeatherReportRepository};
+
+pub struct WeatherReportService<'a> {
+    pub repository: &'a mut dyn WeatherReportRepository,
+}
+
+impl<'a> WeatherReportService<'a> {
+    pub fn new(repository: &'a mut dyn WeatherReportRepository) -> Self {
+        Self { repository }
+    }
+
+    pub fn create_report(&mut self, report: WeatherReport) {
+        self.repository.save(report.report_id.clone(), report);
+    }
+
+    pub fn get_report_by_id(&self, report_id: &str) -> Option<&WeatherReport> {
+        self.repository.find_by_id(&report_id.to_string())
+    }
+
+    pub fn get_all_reports(&self) -> Vec<&WeatherReport> {
+        self.repository.find_all()
+    }
+
+    pub fn delete_report(&mut self, report_id: &str) {
+        self.repository.delete(&report_id.to_string());
+    }
+}
 
